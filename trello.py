65f72a95374d1f95456a8a03a6c10eca6cdd54b8
onefourfive <derek.sorenson@outlook.com>
2019-09-24 10:20:57 -0700

    added gitignore, some functions

--- trello.py
+++ trello.py
@@ -13,9 +13,9 @@ config.read(cfgFile)
 apikey = config.get('id', 'key')
 apitoken = config.get('id', 'token')
 
-def getBoards(key,token):
+def getBoards():
 	reqUrl = 'https://api.trello.com/1/members/me/boards?fields=name,url'
-	payload = {'key':key, 'token':token}
+	payload = {'key':apikey, 'token':apitoken}
 	boards = requests.get(reqUrl, params=payload)
 	#print boards.text
 	return boards.json()
@@ -27,7 +27,35 @@ def getBoardID(boardJson, reqBoard):
 		elif board == max(range(len(boards))):
 			return 0
 
-	
-boards = getBoards(apikey,apitoken)
+def getBoardLists(boardID):
+	reqUrl = "https://api.trello.com/1/boards/" + boardID + "/lists?&filter=open&fields=all"
+	payload = {
+		'fields': 'name,id', 
+		'card_fields': 'name,id', 
+		'cards': 'all', 
+		'key':apikey, 
+		'token':apitoken
+		}
+	lists = requests.get(reqUrl, params=payload)
+	return lists.json()
 
-print(getBoardID(boards, "ANA Media"))
+def addCard(listId, cardName, cardDesc):
+	reqUrl = "https://api.trello.com/1/cards"
+	payload = {
+		'name': cardName,
+		'desc': cardDesc,
+		'idList': listId,
+		'key':apikey, 
+		'token':apitoken
+	}
+	cardAdd = requests.post(reqUrl, params=payload)
+	return cardAdd.json()
+
+boards = getBoards()
+print(json.dumps(boards, indent=4))
+print(boards[3]["name"])
+
+#print(getBoardLists(boards[3]["id"]))
+print(json.dumps(getBoardLists(boards[3]["id"]), indent=4))
+
+#print(getBoardID(boards, "ANA Media"))
